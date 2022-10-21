1043a0b8c441a212c5c45addc66601844ac0fe21
mariam1378 <30564743+mariam1378@users.noreply.github.com>
2022-10-21 09:09:19 +0200

    Add files via upload

---
+++ GccApplication1.c
@@ -0,0 +1,17 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 5/25/2022 10:42:50 AM
+ *  Author: DELL
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
